29be88c010baf4013d6c5ff9e50eae97f4c49f1b
Larissa <larissax55@gmail.com>
2014-05-18 16:50:05 -0300

    Teste triango adiciondo um assert em equilatero teste falhando

--- tests/TrianguloTest.java
+++ tests/TrianguloTest.java
@@ -8,8 +8,9 @@ public class TrianguloTest {
 	
 	@Test
 	public void testEquilatero() {
-		
 		String tipoEquilatero = t.verificaTriangulo(3,3,3);
+		assertEquals(tipoEquilatero, "Equilátero");
+	    tipoEquilatero = t.verificaTriangulo(4,4,4);
 		assertEquals(tipoEquilatero, "Equilátero");
 	
 	}
@@ -19,6 +20,7 @@ public class TrianguloTest {
 		String tipoIsoceles = t.verificaTriangulo(2,3,3);
 		assertEquals(tipoIsoceles, "Isoceles");
 	}
+	
 	@Test
 	public void testScaleno() {
 		
