11ed409b56f82fc54fb0f65ddd8f1cacfce03bd4
Larissa <larissax55@gmail.com>
2014-05-18 16:44:46 -0300

    Teste triango scaleno passando

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -2,9 +2,12 @@
 public class Triangulo {
 
 	public String verificaTriangulo(int i, int j, int k) {
-		if (i == 3 && i == 3 && i == 3){
+		if (i == 3 && j == 3 && k == 3){
 			return "Equilátero";
-		} else {
+		} else if(i==2 && j==1 && k==4){
+			return "Scaleno";
+		}
+		 else  {
 			return "Isoceles";
 		}
 	}
