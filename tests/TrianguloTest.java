4ef689a9daa5fb745cbbbf2795c029bf4e437786
Larissa <larissax55@gmail.com>
2014-05-18 18:00:21 -0300

    colocando o setUp

--- tests/TrianguloTest.java
+++ tests/TrianguloTest.java
@@ -1,62 +1,64 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-
 public class TrianguloTest {
+
+	Triangulo t;
 	
-	Triangulo t = new Triangulo();
-	
+	@Before
+	public void setUp(){
+	  t = new Triangulo();
+	}
 	
 	@Test
 	public void testEquilatero() {
-		String tipoEquilatero = t.retornaTipoTriangulo(3,3,3);
+		String tipoEquilatero = t.retornaTipoTriangulo(3, 3, 3);
 		assertEquals(tipoEquilatero, "Equilátero");
-	    tipoEquilatero = t.retornaTipoTriangulo(4,4,4);
+		tipoEquilatero = t.retornaTipoTriangulo(4, 4, 4);
 		assertEquals(tipoEquilatero, "Equilátero");
 	}
-	
+
 	@Test
 	public void testIsoseles() {
-		String tipoIsoceles = t.retornaTipoTriangulo(2,3,3);
+		String tipoIsoceles = t.retornaTipoTriangulo(2, 3, 3);
 		assertEquals(tipoIsoceles, "Isoceles");
-		
-		tipoIsoceles = t.retornaTipoTriangulo(4,4,10);
+
+		tipoIsoceles = t.retornaTipoTriangulo(4, 4, 10);
 		assertEquals(tipoIsoceles, "Isoceles");
-		
-		
-		tipoIsoceles = t.retornaTipoTriangulo(5,8,5);
+
+		tipoIsoceles = t.retornaTipoTriangulo(5, 8, 5);
 		assertEquals(tipoIsoceles, "Isoceles");
 	}
-	
+
 	@Test
 	public void testScaleno() {
-		
+
 		String tipoScaleno = t.retornaTipoTriangulo(2, 1, 4);
 		assertEquals("Scaleno", tipoScaleno);
-		
+
 	}
-	
+
 	@Test
-	public void testVerificaTriangulo() throws Exception  {
-	 
-	boolean valido = t.verificaTriangulo(3, 3, 3);
-	 assertTrue(valido);
-	 
-	 valido = t.verificaTriangulo(5, 6, 10);
-	 assertTrue(valido);
-	 
+	public void testVerificaTriangulo() throws Exception {
+
+		boolean valido = t.verificaTriangulo(3, 3, 3);
+		assertTrue(valido);
+
+		valido = t.verificaTriangulo(5, 6, 10);
+		assertTrue(valido);
+
 	}
-	
+
 	@Test
-	public void veficarTrianguloInvalido(){
-		try{
-		  t.verificaTriangulo(1, 2, 5);
-		  fail("ERRROO");
-		}catch(Exception e){
-			
+	public void veficarTrianguloInvalido() {
+		try {
+			t.verificaTriangulo(1, 2, 5);
+			fail("ERRROO");
+		} catch (Exception e) {
+
 		}
 	}
-	
 
 }
