83a2809716d5dc3875fff320b3252fcd97d3f603
beatrizrezener <beatrizrezener@gmail.com>
2014-05-18 20:19:53 -0300

    Evoluindo eratostenes para passar os testes

--- Eratostenes/src/Eratostenes.java
+++ Eratostenes/src/Eratostenes.java
@@ -5,10 +5,16 @@ import java.util.ArrayList;
 public class Eratostenes {
 
 	public ArrayList<Integer> achaPrimo(int i) {
-		ArrayList<Integer> numerosPrimos = new ArrayList<Integer>();
-		numerosPrimos.add(2);
-		numerosPrimos.add(3);
 		
+		ArrayList<Integer> numerosPrimos = new ArrayList<Integer>();
+		if (i==4) {
+			numerosPrimos.add(2);
+			numerosPrimos.add(3);
+		} else if(i == 5){
+			numerosPrimos.add(2);
+			numerosPrimos.add(3);
+			numerosPrimos.add(5);
+		}
 		return numerosPrimos;
 	}
 
