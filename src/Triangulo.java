45070acff20a811e0c5ae2c254de2030d196114f
Larissa <larissax55@gmail.com>
2014-05-18 17:28:18 -0300

    Refatorando o nome da função

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,19 +1,22 @@
 
 public class Triangulo {
 
-	public String verificaTriangulo(int i, int j, int k) {
+	public String retornaTipoTriangulo(int i, int j, int k) {
 		if (i == j && i == k && j == k){
 			return "Equilátero";
 		}
 	     if(i != j && j != k  && k !=i){
 			return "Scaleno";
 		} 
+	     
 	     if(i == j && k != j){
 			return "Isoceles";
 		 }
+	     
 	      else if(i == k && k != j){
 				return "Isoceles";
 	     }
+	     
 	      else if(j == k &&  i != k){
 			return "Isoceles"; 
 		}
--- tests/TrianguloTest.java
+++ tests/TrianguloTest.java
@@ -4,32 +4,36 @@ import org.junit.Test;
 
 
 public class TrianguloTest {
+	
 	Triangulo t = new Triangulo();
+	
 	
 	@Test
 	public void testEquilatero() {
-		String tipoEquilatero = t.verificaTriangulo(3,3,3);
+		String tipoEquilatero = t.retornaTipoTriangulo(3,3,3);
 		assertEquals(tipoEquilatero, "Equilátero");
-	    tipoEquilatero = t.verificaTriangulo(4,4,4);
+	    tipoEquilatero = t.retornaTipoTriangulo(4,4,4);
 		assertEquals(tipoEquilatero, "Equilátero");
 	}
 	
 	@Test
 	public void testIsoseles() {
-		String tipoIsoceles = t.verificaTriangulo(2,3,3);
+		String tipoIsoceles = t.retornaTipoTriangulo(2,3,3);
 		assertEquals(tipoIsoceles, "Isoceles");
 		
-		tipoIsoceles = t.verificaTriangulo(4,4,10);
+		tipoIsoceles = t.retornaTipoTriangulo(4,4,10);
 		assertEquals(tipoIsoceles, "Isoceles");
 		
-		tipoIsoceles = t.verificaTriangulo(5,8,5);
+		
+		tipoIsoceles = t.retornaTipoTriangulo(5,8,5);
 		assertEquals(tipoIsoceles, "Isoceles");
 	}
 	
 	@Test
 	public void testScaleno() {
 		
-		String tipoScaleno = t.verificaTriangulo(2, 1, 4);
+		
+		String tipoScaleno = t.retornaTipoTriangulo(2, 1, 4);
 		assertEquals("Scaleno", tipoScaleno);
 		
 	}
