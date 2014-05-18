39203ca18c73b607bf21533b1b48e28bd2e34384
beatrizrezener <beatrizrezener@gmail.com>
2014-05-18 19:59:18 -0300

    Criação de classe de teste eratostenes

---
+++ Eratostenes/tests/eratostenesTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class eratostenesTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
