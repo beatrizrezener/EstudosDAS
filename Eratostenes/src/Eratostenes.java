3148de873ef9f629b7cc66241dc88ce2c2c10dbb
Larissa <larissax55@gmail.com>
2014-05-18 21:19:50 -0300

    Refatorando a função Erastostenes

--- Eratostenes/src/Eratostenes.java
+++ Eratostenes/src/Eratostenes.java
@@ -5,22 +5,37 @@ import java.util.ArrayList;
 public class Eratostenes {
 
 	public ArrayList<Integer> achaPrimo(int i) {
-		
+
 		ArrayList<Integer> numerosRecebidos = new ArrayList<Integer>();
-		
-		for(int cont = 2 ; cont <= i ; cont ++){
-		   numerosRecebidos.add(cont);
+
+		for (int cont = 2; cont <= i; cont++) {
+			numerosRecebidos.add(cont);
 		}
-		
-		ArrayList<Integer>  numerosPrimos = new ArrayList<Integer> ();
-		numerosPrimos.add(2);
-		for(int valorCorrente : numerosRecebidos ){
-			  
-			if(valorCorrente%2 != 0 && valorCorrente != 9  && valorCorrente != 15){
-				  numerosPrimos.add(valorCorrente);
-			  }
+
+		ArrayList<Integer> numerosPrimos = new ArrayList<Integer>();
+
+		for (int valorCorrente : numerosRecebidos) {
+			if (valorCorrente == 2) {
+				numerosPrimos.add(2);
+			}
+			if (valorCorrente % 2 != 0 && valorCorrente != 9
+					&& valorCorrente != 15) {
+				numerosPrimos.add(valorCorrente);
+			}
 		}
-		return numerosPrimos;
+
+		ArrayList<Integer> numerosNaoMultiplosDeDois = new ArrayList<Integer>();
+
+		for (int valorCorrente : numerosPrimos) {
+			if (valorCorrente == 3) {
+				numerosNaoMultiplosDeDois.add(3);
+			}
+			if (valorCorrente % 3 != 0) {
+				numerosNaoMultiplosDeDois.add(valorCorrente);
+			}
+		}
+
+		return numerosNaoMultiplosDeDois;
 	}
 
 }
